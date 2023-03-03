fc485900eddf7f40bc98e67480ecbe2980ca67c4
Lilian Oliveira <lilian26.or@gmail.com>
2023-03-03 16:45:30 -0300

    Alterada página CustomerForm

--- src/components/CustomerForm/index.tsx
+++ src/components/CustomerForm/index.tsx
@@ -1,3 +1,5 @@
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import { useFormik } from 'formik';
 import {
   Box,
@@ -5,7 +7,9 @@ import {
   FormControl,
   FormLabel,
   Input,
-  VStack
+  VStack,
+  FormErrorMessage,
+  Select
 } from '@chakra-ui/react';
 import styles from './CustomerForm.module.scss';
 
@@ -26,6 +30,7 @@ export default function CustomerForm() {
   });
 
   const inputBackground = { background: 'gray.200' };
+  const options = ['Feminino', 'Masculino'];
 
   return (
     <div>
@@ -34,7 +39,7 @@ export default function CustomerForm() {
       <Flex className={styles.flexName}>
         <Box className={styles.box}>
           <form onSubmit={formik.handleSubmit}>
-            <VStack spacing={4} align="flex-start">
+            <VStack className={styles.vStack}>
               <FormControl>
                 <FormLabel htmlFor="name">Nome Completo*</FormLabel>
                 <Input
@@ -48,7 +53,7 @@ export default function CustomerForm() {
                 />
               </FormControl>
               <Flex className={styles.flex}>
-                <FormControl className={styles.formControl}>
+                <FormControl>
                   <FormLabel htmlFor="cpf">Cpf*</FormLabel>
                   <Input
                     sx={inputBackground}
@@ -56,11 +61,27 @@ export default function CustomerForm() {
                     name="cpf"
                     type="cpf"
                     variant="filled"
-                    onChange={formik.handleChange}
+                    maxLength={11}
+                    onChange={e => {
+                      if (e.target.value.length !== 11) {
+                        formik.setFieldError(
+                          'cpf',
+                          'CPF deve ter 11 caracteres'
+                        );
+                      } else {
+                        formik.setFieldError('cpf', undefined);
+                      }
+                      formik.handleChange(e);
+                    }}
                     value={formik.values.cpf}
+                    isInvalid={!!(formik.errors.cpf && formik.touched.cpf)}
+                    errorBorderColor="red.300"
                   />
+                  {formik.errors.cpf && formik.touched.cpf && (
+                    <FormErrorMessage>{formik.errors.cpf}</FormErrorMessage>
+                  )}
                 </FormControl>
-                <FormControl className={styles.formControl}>
+                <FormControl>
                   <FormLabel htmlFor="rg">Rg</FormLabel>
                   <Input
                     sx={inputBackground}
@@ -74,36 +95,51 @@ export default function CustomerForm() {
                 </FormControl>
               </Flex>
               <Flex className={styles.flex}>
-                <FormControl className={styles.formControl}>
+                <FormControl>
                   <FormLabel htmlFor="sexo">Sexo*</FormLabel>
-                  <Input
+                  <Select
                     sx={inputBackground}
                     id="sexo"
                     name="sexo"
-                    type="sexo"
+                    placeholder="Selecione uma opção"
                     variant="filled"
                     onChange={formik.handleChange}
                     value={formik.values.sexo}
-                  />
+                  >
+                    {options.map(option => (
+                      <option key={option} value={option.toLowerCase()}>
+                        {option}
+                      </option>
+                    ))}
+                  </Select>
                 </FormControl>
-                <FormControl className={styles.formControl}>
+                <FormControl>
                   <FormLabel htmlFor="datanascimento">
                     Data de nascimento
                   </FormLabel>
-                  <Input
-                    sx={inputBackground}
+                  <DatePicker
                     id="datanascimento"
                     name="datanascimento"
-                    type="datanascimento"
-                    variant="filled"
-                    onChange={formik.handleChange}
-                    value={formik.values.datanascimento}
+                    selected={
+                      formik.values.datanascimento
+                        ? new Date(formik.values.datanascimento)
+                        : null
+                    }
+                    onChange={(date: Date) =>
+                      formik.setFieldValue('datanascimento', date)
+                    }
+                    dateFormat="dd/MM/yyyy"
+                    showYearDropdown
+                    yearDropdownItemNumber={100}
+                    scrollableYearDropdown
+                    showMonthDropdown
+                    className={styles.datepicker}
                   />
                 </FormControl>
               </Flex>
 
               <Flex className={styles.flex}>
-                <FormControl className={styles.formControl}>
+                <FormControl>
                   <FormLabel htmlFor="telefone">Telefone*</FormLabel>
                   <Input
                     sx={inputBackground}
@@ -115,7 +151,7 @@ export default function CustomerForm() {
                     value={formik.values.telefone}
                   />
                 </FormControl>
-                <FormControl className={styles.formControl}>
+                <FormControl>
                   <FormLabel htmlFor="telefoneadicional">
                     Telefone Adicional
                   </FormLabel>
