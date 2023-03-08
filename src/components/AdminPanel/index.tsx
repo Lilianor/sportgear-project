8829c7eda8e27e1aa698bcacfc63d685a425259c
Lilian Oliveira <lilian26.or@gmail.com>
2023-03-08 20:53:33 -0300

    Alteração painel adm

--- src/components/AdminPanel/index.tsx
+++ src/components/AdminPanel/index.tsx
@@ -1,85 +1,168 @@
-import Link from "../Link";
-
-
+import { useState } from 'react';
 import {
-  
-  Button
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  Button,
+  Input,
+  useToast,
+  Spinner,
+  Flex,
+  ModalCloseButton,
+  FormControl,
+  FormLabel
 } from '@chakra-ui/react';
 import Table from 'react-bootstrap/Table';
-import styles from "./AdminPanel.module.scss";
+import Link from '../Link';
+import styles from './AdminPanel.module.scss';
+
+interface CategoryProps {
+  category: 'product' | 'category' | 'user' | 'card';
+}
+
+function translateCategory(category: any) {
+  if (category === 'product') return 'Produto';
+  if (category === 'category') return 'Categoria';
+  if (category === 'user') return 'Cliente';
+  if (category === 'card') return 'Pedido';
+}
 
 export default function AdminPanel() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeCategory, setActiveCategory] =
+    useState<CategoryProps['category']>('product');
+
+  // const fetchUrl = `http://localhost:5000/${activeCategory}`;
+
   return (
     <main>
-    <div className={styles.text}>
-      <h1>Painel Administrativo</h1>
-      <h3>Bem vindo, admin!</h3>
-    </div>
-    <div className={styles.links}>
-      <Link texto="Produtos" redirect="/" className={styles.btn} />
-      <Link texto="Categorias" redirect="/" className={styles.btn} />
-      <Link texto="Clientes" redirect="/" className={styles.btn} />
-      <Link texto="Pedidos" redirect="/" className={styles.btn} />
-    </div>
-    <div>
-      <Button className={styles.btnstyle}> Cadastrar novo produto</Button>
-    </div>
+      <div className={styles.text}>
+        <h1>Painel Administrativo</h1>
+        <h3>Bem vindo, admin!</h3>
+      </div>
+      <div className={styles.links}>
+        <button
+          onClick={() => setActiveCategory('product')}
+          className={`${styles.btn} ${
+            activeCategory === 'product' && styles.activeButton
+          }`}
+        >
+          Produtos
+        </button>
+        <button
+          onClick={() => setActiveCategory('category')}
+          className={`${styles.btn} ${
+            activeCategory === 'category' && styles.activeButton
+          }`}
+        >
+          Categorias
+        </button>
+        <button
+          onClick={() => setActiveCategory('user')}
+          className={`${styles.btn} ${
+            activeCategory === 'user' && styles.activeButton
+          }`}
+        >
+          Clientes
+        </button>
+        <button
+          onClick={() => setActiveCategory('card')}
+          className={`${styles.btn} ${
+            activeCategory === 'card' && styles.activeButton
+          }`}
+        >
+          Pedidos
+        </button>
+      </div>
+      <div>
+        <Button className={styles.btnstyle} onClick={() => setIsOpen(true)}>
+          Cadastrar {translateCategory(activeCategory)}
+        </Button>
+      </div>
 
-    <div className={styles.table}>
-      <Table striped="columns" className={styles.responsiveTable}>
-        <thead>
-          <tr>
-            <th>Código</th>
-            <th>Nome</th>
-            <th>Categoria</th>
-            <th>Preço</th>
-            <th className={styles.actions}>Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>
-              <div className={styles.buttons}>
-                <div className={styles.button}>
-                  <button>Ver detalhes</button>
+      <div className={styles.table}>
+        <Table striped="columns" className={styles.responsiveTable}>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Nome</th>
+              <th>Categoria</th>
+              <th>Preço</th>
+              <th className={styles.actions}>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>
+                <div className={styles.buttons}>
+                  <div className={styles.button}>
+                    <button>Editar</button>
+                  </div>
+                  <div className={styles.button}>
+                    <button>Excluir</button>
+                  </div>
                 </div>
-                <div className={styles.button}>
-                  <button>Excluir</button>
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>
+                <div className={styles.buttons}>
+                  <button className={styles.button}>Editar</button>
+                  <button className={styles.button}>Excluir</button>
                 </div>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>
-              <div className={styles.buttons}>
-                <button className={styles.button}>Ver detalhes</button>
-                <button className={styles.button}>Excluir</button>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>
-              <div className={styles.buttons}>
-                <button className={styles.button}>Ver detalhes</button>
-                <button className={styles.button}>Excluir</button>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-    </div>
-  </main>
+              </td>
+            </tr>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>
+                <div className={styles.buttons}>
+                  <button className={styles.button}>Editar</button>
+                  <button className={styles.button}>Excluir</button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Criar Cadastro</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              
+              <Input className={styles.input} placeholder="Id" />
+            </FormControl>
+
+            <FormControl mt={4}>
+              
+              <Input className={styles.input} placeholder="Nome" />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3}>
+              Salvar
+            </Button>
+            <Button onClick={() => setIsOpen(false)}>Cancelar</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </main>
   );
 }
--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,12 +1,10 @@
 import AdminPanel from '../components/AdminPanel';
 import Header from '../components/Header';
-import Navigation from "../components/Navigation";
 
 export default function Admin() {
   return (
     <>
       <Header isAdmin={true} />
-      <Navigation title="PRODUTOS" />
       <AdminPanel />
     </>
   );
