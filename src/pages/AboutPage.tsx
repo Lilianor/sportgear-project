a6ba15b738692be5dda892d559188022360c6652
filipecalm <filipecalm@gmail.com>
2023-03-14 03:51:03 -0300

    Imagem da página About

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,6 +1,6 @@
 import Header from '../components/Header';
 import Navigation from '../components/Navigation';
-import About from '../components/About';
+import About from '../components/AboutPage';
 import Footer from '../components/Footer';
 
 export default function AboutPage() {
